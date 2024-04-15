37dce4fdc25ae5aefe4da2c97b0560048c1096d6
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2024-04-15 17:20:24 +0900

    details in <Config>

--- app/src/Config.tsx
+++ app/src/Config.tsx
@@ -68,81 +68,89 @@ export const Config: React.FC<Props> = ({
   const queryIds = querys.map((record) => record.id)
   return (
     <div style={style}>
-      <div>
-        x(target)=
-        <List
-          items={targetIds}
-          index={targetIndex}
-          onChange={onChangeTargetIndex}
+      <details open>
+        <summary></summary>
+        <div>
+          x(target)=
+          <List
+            items={targetIds}
+            index={targetIndex}
+            onChange={onChangeTargetIndex}
+          />
+          len={targets[targetIndex]?.len}
+        </div>
+        <div>
+          y(query)=
+          <List
+            items={queryIds}
+            index={queryIndex}
+            onChange={onChangeQueryIndex}
+          />
+          len={querys[queryIndex]?.len}
+        </div>
+        <button onClick={onAdd}>add</button>
+        live
+        <CheckBox value={live} onChange={onChangeLive} />
+        <div>k={k}</div>
+        <Slider value={k} onChange={onChangeK} min={1} max={100} />
+        <div>freqLow={freqLow}</div>
+        <Slider
+          value={freqLow}
+          onChange={onChangeFreqLow}
+          min={1}
+          max={freqUp}
         />
-        len={targets[targetIndex]?.len}
-      </div>
-      <div>
-        y(query)=
-        <List
-          items={queryIds}
-          index={queryIndex}
-          onChange={onChangeQueryIndex}
+        <div>freqUp={freqUp}</div>
+        <Slider
+          value={freqUp}
+          onChange={onChangeFreqUp}
+          min={freqLow}
+          max={100}
         />
-        len={querys[queryIndex]?.len}
-      </div>
-      <button onClick={onAdd}>add</button>
-      live
-      <CheckBox value={live} onChange={onChangeLive} />
-      <div>k={k}</div>
-      <Slider value={k} onChange={onChangeK} min={1} max={100} />
-      <div>freqLow={freqLow}</div>
-      <Slider value={freqLow} onChange={onChangeFreqLow} min={1} max={freqUp} />
-      <div>freqUp={freqUp}</div>
-      <Slider
-        value={freqUp}
-        onChange={onChangeFreqUp}
-        min={freqLow}
-        max={100}
-      />
-      <div>
-        match revcomp
-        <CheckBox value={revcomp} onChange={onChangeRevcomp} />
-      </div>
-      <div>cx={region.center.x.toFixed(0)}</div>
-      <div>cy={region.center.y.toFixed(0)}</div>
-      <div>scale={region.scale.toFixed(3)}</div>
-      <div>
-        <input
-          type="color"
-          value={color}
-          onChange={(e) => onChangeColor(e.target.value)}
-        />
-      </div>
-      <button onClick={onSave}>save</button>
-      {plots.map((plot, i) => {
-        return (
-          <div key={plot.key}>
-            Plot#{i}
-            <CheckBox
-              value={plot.active}
-              onChange={(active) => {
-                const newPlots = [...plots]
-                const newPlot: Plot = {
-                  ...plot,
-                  active,
-                }
-                newPlots[i] = newPlot
-                onChangePlots(newPlots)
-              }}
-            />
-            <button
-              onClick={() => {
-                const newPlots = [...plots]
-                newPlots.splice(i, 1)
-                onChangePlots(newPlots)
-              }}
-            >
-              Remove
-            </button>
-          </div>
-        )
-      })}
+        <div>
+          match revcomp
+          <CheckBox value={revcomp} onChange={onChangeRevcomp} />
+        </div>
+        <div>cx={region.center.x.toFixed(0)}</div>
+        <div>cy={region.center.y.toFixed(0)}</div>
+        <div>scale={region.scale.toFixed(3)}</div>
+        <div>
+          <input
+            type="color"
+            value={color}
+            onChange={(e) => onChangeColor(e.target.value)}
+          />
+        </div>
+        <button onClick={onSave}>save</button>
+        {plots.map((plot, i) => {
+          return (
+            <div key={plot.key}>
+              Plot#{i}
+              <CheckBox
+                value={plot.active}
+                onChange={(active) => {
+                  const newPlots = [...plots]
+                  const newPlot: Plot = {
+                    ...plot,
+                    active,
+                  }
+                  newPlots[i] = newPlot
+                  onChangePlots(newPlots)
+                }}
+              />
+              <button
+                onClick={() => {
+                  const newPlots = [...plots]
+                  newPlots.splice(i, 1)
+                  onChangePlots(newPlots)
+                }}
+              >
+                Remove
+              </button>
+            </div>
+          )
+        })}
+      </details>
     </div>
   )
 }
