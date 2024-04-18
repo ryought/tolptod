30c51484cd61c564a3ff1ab3af46f69bdf2ec190
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2024-04-18 10:58:19 +0900

    bitvec rank worked

--- wavelet/bitvec/v2.go
+++ wavelet/bitvec/v2.go
@@ -1,10 +1,21 @@
 package bitvec
 
-import "fmt"
+import (
+	"fmt"
+	"math/bits"
+)
 
+const L = 256
+const S = 8
+const U = L / BYTE
+const BYTE = 8
+
+// Use ~2n bits for ~n bits supporting rank operation
 type BitVecV2 struct {
-	size   int
-	chunks []byte
+	size   int    // n
+	chunks []byte // 1bit  x n           = n
+	rankL  []int  // 64bit x n/L (L=256) = n/4
+	rankS  []byte // 8bit  x n/S (S=8)   = n
 }
 
 // Perform div a/b then ceil.
@@ -16,7 +27,9 @@ func div(a int, b int) int {
 func NewV2(size int) BitVecV2 {
 	return BitVecV2{
 		size:   size,
-		chunks: make([]byte, div(size, 8)),
+		chunks: make([]byte, div(size, BYTE)),
+		rankL:  make([]int, div(size, L)),
+		rankS:  make([]byte, div(size, S)),
 	}
 }
 
@@ -25,16 +38,25 @@ func (bv BitVecV2) Size() int {
 }
 
 func (bv BitVecV2) Debug() {
+	fmt.Printf("********\n")
 	fmt.Printf("size=%d\n", bv.size)
-	fmt.Printf("i\t01234567\n")
 	for i := range bv.chunks {
-		fmt.Printf("%d\t%08b\n", i, bv.chunks[i])
+		if i%U == 0 {
+			fmt.Printf("%d\t%08b\t%d\t%d\n", i, bv.chunks[i], bv.rankL[i/U], bv.rankS[i])
+		} else {
+			fmt.Printf("%d\t%08b\t-\t%d\n", i, bv.chunks[i], bv.rankS[i])
+		}
 	}
+	fmt.Printf("********\n")
+}
+
+// Get i/8 and i%8
+func address(i int) (int, int) {
+	return i >> 3, i & 0b111
 }
 
 func (bv BitVecV2) Set(i int, x byte) {
-	index := i >> 3
-	offset := i & 0b111
+	index, offset := address(i)
 	chunk := bv.chunks[index]
 	if x == 0 {
 		chunk = chunk & ^(1 << offset)
@@ -45,8 +67,40 @@ func (bv BitVecV2) Set(i int, x byte) {
 }
 
 func (bv BitVecV2) Get(i int) byte {
-	index := i >> 3
-	offset := i & 0b111
+	index, offset := address(i)
 	chunk := bv.chunks[index]
 	return (chunk >> offset) & 1
+}
+
+func (bv BitVecV2) UpdateRank() {
+	rank := 0
+	rankL := 0
+	for k, chunk := range bv.chunks {
+		if k%U == 0 {
+			bv.rankL[k/U] = rank
+			rankL = rank
+		}
+		bv.rankS[k] = byte(rank - rankL)
+		rank += BYTE - bits.OnesCount8(chunk)
+	}
+}
+
+// Count zero in first [0:offset) bits
+//
+// 0b_0010_0101 b
+// ------|-<--> [0:offset)
+// offset=4
+//
+// 0b_1111_0000 mask
+func countZeros(b byte, offset int) int {
+	mask := byte(^((1 << offset) - 1))
+	return BYTE - bits.OnesCount8(b|mask)
+}
+
+// Count zeros in [0:i)
+func (bv BitVecV2) Rank(i int) int {
+	index, offset := address(i)
+	chunk := bv.chunks[index]
+	rank := countZeros(chunk, offset)
+	return bv.rankL[index/U] + int(bv.rankS[index]) + rank
 }
--- wavelet/bitvec/v2_test.go
+++ wavelet/bitvec/v2_test.go
@@ -5,7 +5,31 @@ import (
 	"testing"
 )
 
-func TestV2(t *testing.T) {
+func TestCountZeros(t *testing.T) {
+	tests := []struct {
+		offset int
+		count  int
+	}{
+		{0, 0}, // [0,0) is empty
+		{1, 0}, // [0,1) = 1
+		{2, 1}, // [0,2) = 01
+		{3, 1}, // [0,3) = 101
+		{4, 2},
+		{5, 3}, // [0,5) = 00101
+		{6, 3}, // [0,6) = 100101
+		{7, 4}, // [0,7) = 0100101
+		{8, 5}, // [0,8) = 00100101
+	}
+
+	for _, test := range tests {
+		count := countZeros(0b_0010_0101, test.offset)
+		if count != test.count {
+			t.Error()
+		}
+	}
+}
+
+func TestV2Bit(t *testing.T) {
 	v := NewV2(16)
 	v.Set(0, 1)
 	v.Set(5, 1)
@@ -37,5 +61,53 @@ func TestV2(t *testing.T) {
 
 	if !bytes.Equal(v.chunks, []byte{0b10000001, 0b00001000}) {
 		t.Error()
+	}
+
+	v.UpdateRank()
+	v.Debug()
+}
+
+func TestV2RankSmall(t *testing.T) {
+	v := NewV2(33)
+	v.Set(0, 1)
+	v.Set(5, 1)
+	v.Set(7, 1)
+	v.Set(9, 1)
+	v.Set(11, 1)
+	v.Set(15, 1)
+	v.Set(20, 1)
+	v.Set(25, 1)
+	v.Set(32, 1)
+	v.UpdateRank()
+	v.Debug()
+
+	tests := []struct {
+		i int
+		r int
+	}{
+		{0, 0},
+		{1, 0},
+		{2, 1},
+		{6, 4},
+	}
+	for _, test := range tests {
+		r := v.Rank(test.i)
+		t.Log(test.i, test.r, r)
+		if r != test.r {
+			t.Error()
+		}
+	}
+}
+
+func TestV2RankLarge(t *testing.T) {
+	v := NewV2(555)
+	v.UpdateRank()
+	v.Debug()
+
+	// Since all bits are zero, rank(i)=i for all i.
+	for i := 0; i < 555; i++ {
+		if v.Rank(i) != i {
+			t.Error()
+		}
 	}
 }
